d1323fe2656213b39431e15888c373184e19f794
Maks Kremez <mr.kremez@gmail.com>
2016-08-07 21:10:48 +0300

    [screencast_15] Echo server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 var http = require('http');
+var url = require('url');
 
-var server = new http.Server();
+var server = new http.Server(function(req, res) {
+    console.log(req.method, req.url);
+
+    var urlParsed = url.parse(req.url, true);
+    console.log(req.headers);
+
+    if (urlParsed.pathname == '/echo' && urlParsed.query.message) {
+        res.setHeader('Cache-control', 'no-cache');
+        res.end(urlParsed.query.message);
+    } else {
+        res.statusCode = 404;
+        res.end('Page not found');
+    }
+});
 
 server.listen(1337, '127.0.0.1');
-
-var counter = 0;
-server.on('request', function(req, res) {
-    res.end('Hello, world!' + ++counter);
-});
