12517f9a2083c22e80e2c05a48fdf0baa3474a5d
Maks Kremez <mr.kremez@gmail.com>
2016-08-13 14:18:44 +0300

    [screencast_26] Pipe

--- write_stream_server/pipe.js
+++ write_stream_server/pipe.js
@@ -1,29 +1,31 @@
 var http = require('http');
 var fs = require('fs');
 
-new http.Server(function(req,res) {
-   if (req.url == '/big.html') {
-       var file = new fs.ReadStream('big.html');
-       sendFile(file, res);
-   }
+new http.Server(function (req, res) {
+    if (req.url == '/big.html') {
+        var file = new fs.ReadStream('big.html');
+        sendFile(file, res);
+    }
 }).listen(3000);
 
 function sendFile(file, res) {
-    file.on('readable', write);
+    file.pipe(res);
+    file.pipe(process.stdout);
 
-    function write() {
-        var fileContent = file.read();
-
-        if (fileContent && !res.write(fileContent)) {
-            file.removeListener('readable', write);
-
-            res.once('drain', function() {
-                file.on('readable', write);
-                write();
-            });
-        }
-    }
-    file.on('end', function() {
-       res.end();
+    file.on('error', function (err) {
+        res.statusCode = 500;
+        res.end('Server Error');
+        console.error(err);
     });
+
+    file
+        .on('open', function () {
+            console.log('open');
+        })
+        .on('close', function () {
+            console.log('close');
+        });
+    res.on('close', function () {
+        file.destroy();
+    })
 }
