203e4c8a2c9c1246706f3469e4925681d402dde4
Maks Kremez <mr.kremez@gmail.com>
2016-08-14 19:42:56 +0300

    [screencast_36]  Add script for creation test database (async lib)

--- createDb.js
+++ createDb.js
@@ -1,14 +1,42 @@
-var User = require('./models/user').User;
+var mongoose = require('libs/mongoose');
+var async = require('async');
 
-var user = new User({
-    username: "Tester2",
-    password: "secret"
+async.series([
+    open,
+    dropDatabase,
+    requireModels,
+    createUsers
+], function(err, results) {
+    console.log(arguments);
+    mongoose.disconnect();
+    process.exit(err ? 255 : 0);
 });
 
-user.save(function(err, user, affected) {
-    if (err) throw err;
+function open(callback) {
+    mongoose.connection.on('open', callback);
+}
 
-    User.findOne({username: 'Tester2'}, function(err, tester) {
-        console.log(tester);
-    });
-});
+function dropDatabase(callback) {
+    var db = mongoose.connection.db;
+    db.dropDatabase(callback);
+}
+
+function requireModels(callback) {
+    require('models/user');
+
+    async.each(Object.keys(mongoose.models), function(modelName, callback) {
+        mongoose.models[modelName].ensureIndexes(callback);
+    }, callback);
+}
+
+function createUsers(callback) {
+    var users = [
+        {username: 'Vasya', password: '1234'},
+        {username: 'Petya', password: '1234'},
+        {username: 'admin', password: '1234'}
+    ];
+    async.each(users, function(userData, callback) {
+        var user = new mongoose.models.User(userData);
+        user.save(callback);
+    }, callback);
+}
