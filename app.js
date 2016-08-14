f4ed4afea6d47096cbd75bfdb50025173fee53e1
Maks Kremez <mr.kremez@gmail.com>
2016-08-14 22:28:32 +0300

    [screencast_37]  Error handling

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var HttpError = require('error').HttpError;
+var errorhandler = require('errorhandler');
+var log = require('libs/log')(module);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,41 +24,29 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('middleware/sendHttpError'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
-// error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+  // only use in development
+  app.use(errorhandler())
 }
 
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  if (typeof err === 'number') {
+    err = new HttpError(err);
+  }
+  if(err instanceof HttpError) {
+    res.sendHttpError(err);
+  } else {
+    log.error(err);
+    err = new HttpError(500);
+    res.sendHttpError(err);
+  }
 });
-
 
 module.exports = app;
